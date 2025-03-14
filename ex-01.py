14a82f5f102aed4fa63bbd3d89ef7864e0580217
luc <lucaspato7@yahoo.com>
2025-03-13 21:51:18 -0300

    Create ex-01.py

---
+++ ex-01.py
@@ -0,0 +1,18 @@
+#Escreva um algoritmo para ler um valor numérico (do teclado) e escrever (na tela) o seu antecessor
+
+print('qual é o numero?')
+numero = int(input())
+print(numero-1)
+
+#Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo
+print('qual a base do retangulo?')
+numero3 = int(input())
+print('qual o altura do retangulo')
+numero4 = int(input())
+
+
+
+
+vsf= numero3 * numero4
+
+print(vsf)
