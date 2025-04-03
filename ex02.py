0a7dda732f00a07d73f841dae850cdab5221a4e4
lucas <lucaspato7@yahoo.com>
2025-04-03 20:45:21 -0300

    ex02.py

---
+++ ex02.py
@@ -0,0 +1,10 @@
+numero1 = int(input('informe o primeiro numero'))
+numero2 = int(input('informe o segundo numero'))
+numero3 = int(input('informe o terceiro numero'))
+
+if (numero1 > numero2) and (numero1 > numero3):
+ print('o primeiro é maior')
+elif (numero2 > numero1) and (numero2>numero3):
+ print('o segundo segundo é maior')
+else:
+    print('terceiro é maior')
