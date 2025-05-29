2801fe8a345e550f1505993f7435f279bf80ce01
lucas <lucaspato7@yahoo.com>
2025-05-29 14:32:25 -0300

    ex23.py

--- ex23.py
+++ ex23.py
@@ -6,3 +6,15 @@ while senha_digitada != senha_correta:
 if senha_digitada != senha_correta:
     print('Senha errada! Tente novamente')
     print('Acesso Permitido! Bem vindo!')
+
+salario_fixo = float(input("Digite o salário fixo: "))
+vendas = float(input("Digite o valor total das vendas: "))
+
+if vendas <= 1500:
+    comissao = vendas * 0.03
+else:
+    comissao = 1500 * 0.03 + (vendas - 1500) * 0.05
+
+salario_total = salario_fixo + comissao
+
+print(f"Salário total: R$ {salario_total:.2f}")
